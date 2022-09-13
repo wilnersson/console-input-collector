da5ddac3604955ec6b3e31f80d1dbb5addcd194d
Henrik Wilnersson <hw222nq@student.lnu.se>
2022-09-13 08:06:50 +0200

    Add basic structure for automated unit testing

---
+++ test/index.test.js
@@ -0,0 +1,12 @@
+/**
+ * Module for unit testing.
+ *
+ * @author Henrik Wilnersson <hw222nq@student.lnu.se>
+ * @version 1.0.0
+ */
+
+describe('Tests for Jest implementation', () => {
+  test('Testing jest; true should be true', () => {
+    expect(true).toBe(true)
+  })
+})
