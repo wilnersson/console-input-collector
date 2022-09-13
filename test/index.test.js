18491930e12613f8a921570d30724dec8d126df6
Henrik Wilnersson <hw222nq@student.lnu.se>
2022-09-13 16:15:38 +0200

    Add unit testing for IntegerInput

--- test/index.test.js
+++ test/index.test.js
@@ -6,6 +6,7 @@
  */
 
 import { StringInput } from '../src/StringInput.js'
+import { IntegerInput } from '../src/IntegerInput.js'
 
 describe('StringInput', () => {
   test('Supplying "Hello World!" should return the same string.', () => {
@@ -21,5 +22,35 @@ describe('StringInput', () => {
   test('A string longer than maxAnswerLength should be invalid.', () => {
     const stringInput = new StringInput('A much too long string', 5)
     expect(stringInput.isValid()).toBe(false)
+  })
+})
+
+describe('IntegerInput', () => {
+  test('Supplying 35 should return the same number.', () => {
+    const integerInput = new IntegerInput(35)
+    expect(integerInput.getInput()).toBe(35)
+  })
+
+  test('A string should be invalid.', () => {
+    const integerInput = new IntegerInput('Hello World!')
+    expect(integerInput.isValid()).toBe(false)
+  })
+
+  test('A number outside the supplied range should be invalid.', () => {
+    const integerInput = new IntegerInput(35, 1, 34)
+    expect(integerInput.isValid()).toBe(false)
+  })
+
+  test('Supplying a null minValue should work and mean no lower limit to the input value.', () => {
+    const integerInput = new IntegerInput(-9999999, null, 0)
+    expect(integerInput.isValid()).toBe(true)
+  })
+
+  test('Supplying a minValue larger than maxValue should throw an Error.', () => {
+    expect(() => new IntegerInput(35, 100, 1)).toThrow(Error)
+  })
+
+  test('Supplying a string as a maxValue parameter should throw a TypeError.', () => {
+    expect(() => new IntegerInput(1000000000, 0, 'Hello World!')).toThrow(TypeError)
   })
 })
