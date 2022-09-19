e8f327c12d218b8d84c77f588928138eb54d225c
Henrik Wilnersson <hw222nq@student.lnu.se>
2022-09-19 14:18:03 +0200

    Polish output in test-app

--- test-app/index.js
+++ test-app/index.js
@@ -9,13 +9,17 @@ import collector from '../src/index.js'
 
 /**
  * Starts the test-application.
+ *
+ * This is a very simple application that tests the terminal-input-collector package.
+ * It simply asks for input in different ways and finally prints the result.
+ * Please do not share your actual password or information with this application.
  */
 async function start () {
   const collectedInformation = {}
 
   do {
     try {
-      collectedInformation.firstName = await collector.getStringInput('Please enter your first name:', 2)
+      collectedInformation.firstName = await collector.getStringInput('Please enter your first name:', 100)
     } catch (error) {
       if (checkForInvalidInputError(error)) {
         printInvalidInputMessage()
@@ -25,7 +29,7 @@ async function start () {
 
   do {
     try {
-      collectedInformation.lastName = await collector.getStringInput('Please enter your last name:')
+      collectedInformation.lastName = await collector.getStringInput('Please enter your last name:', 100)
     } catch (error) {
       if (checkForInvalidInputError(error)) {
         printInvalidInputMessage()
@@ -57,27 +61,28 @@ async function start () {
 
   do {
     try {
-      collectedInformation.password = await collector.getPasswordInput('Set your password:')
-      const validatedPassword = await collector.getPasswordInput('Enter password again:')
+      collectedInformation.password = await collector.getPasswordInput('Set your password:', 8, 1000)
+      const validatedPassword = await collector.getPasswordInput('Enter password again:', 8, 1000)
 
       if (collectedInformation.password !== validatedPassword) {
-        delete collectedInformation.password
-
         const passwordMatchError = new Error('Passwords do not match.')
         passwordMatchError.name = 'PasswordMatchError'
         throw passwordMatchError
       }
     } catch (error) {
       if (checkForInvalidInputError(error)) {
-        printInvalidInputMessage()
+        printInvalidPasswordMessage()
       }
 
       if (error.name === 'PasswordMatchError') {
         console.log('Passwords do not match, try again...')
       }
+
+      delete collectedInformation.password
     }
   } while (!collectedInformation.password)
 
+  console.log('\n --- Thank you, this was the result ---')
   console.log(collectedInformation)
 }
 
@@ -86,6 +91,13 @@ async function start () {
  */
 function printInvalidInputMessage () {
   console.log('Invalid input, try again...')
+}
+
+/**
+ * Prints the error message on invalid password input.
+ */
+function printInvalidPasswordMessage () {
+  console.log('Invalid input. Password must be at least 8 characters, at most 1000 characters.')
 }
 
 /**
